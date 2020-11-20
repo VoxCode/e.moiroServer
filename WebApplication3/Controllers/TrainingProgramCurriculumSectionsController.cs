2a1da22309b500d84254cfa9ccb4f3426113f3eb
Андрей Кезик <kezik.1992@gmail.com>
2020-11-20 08:09:59 +0300

    v 0.9

--- WebApplication3/Controllers/TrainingProgramCurriculumSectionsController.cs
+++ WebApplication3/Controllers/TrainingProgramCurriculumSectionsController.cs
@@ -28,16 +28,9 @@ namespace e.moiroServer.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<TrainingProgramCurriculumSection>> Get(int id)
+        public async Task<ActionResult<IEnumerable<TrainingProgramCurriculumSection>>> Get(int id)
         {
-            var value = await _context.TrainingProgramCurriculumSections.FindAsync(id);
-
-            if (value == null)
-            {
-                return NotFound();
-            }
-
-            return value;
+            return  await _context.TrainingProgramCurriculumSections.Where(a => a.TrainingProgramId == id).ToListAsync();
         }
 
         [HttpPut]

ff7ef751abc94251747ac06a61ff07479c48ce10
Андрей Кезик <kezik.1992@gmail.com>
2021-02-05 15:59:55 +0300

    v 0.9

---
+++ WebApplication3/Migrations/20210205124515_TrainngProgramAddFormOfEducation.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace e.moiroServer.Migrations
+{
+    public partial class TrainngProgramAddFormOfEducation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FormOfEducationId",
+                table: "TrainingPrograms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainingPrograms_FormOfEducationId",
+                table: "TrainingPrograms",
+                column: "FormOfEducationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TrainingPrograms_FormOfEducations_FormOfEducationId",
+                table: "TrainingPrograms",
+                column: "FormOfEducationId",
+                principalTable: "FormOfEducations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TrainingPrograms_FormOfEducations_FormOfEducationId",
+                table: "TrainingPrograms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TrainingPrograms_FormOfEducationId",
+                table: "TrainingPrograms");
+
+            migrationBuilder.DropColumn(
+                name: "FormOfEducationId",
+                table: "TrainingPrograms");
+        }
+    }
+}

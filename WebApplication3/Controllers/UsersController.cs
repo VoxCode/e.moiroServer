15dcaf384c092c6468cc966187ce8e66263f8f31
Андрей Кезик <kezik.1992@gmail.com>
2021-03-23 16:59:50 +0300

    v 0.9

---
+++ WebApplication3/Controllers/UsersController.cs
@@ -0,0 +1,91 @@
+﻿using e.moiroServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e.moiroServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public UsersController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<object>>> Get()
+        {
+            var tmp = from first in _context.Roles
+                      join second in _context.UserRoles on first.Id equals second.RoleId
+                      join third in _context.Users on second.UserId equals third.Id
+                      select new
+                      {
+                          third.UserName,
+                          third.Email,
+                          first.Name
+
+                      };
+
+            return await tmp.ToListAsync().ConfigureAwait(false);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            var value = await _context.Users.FindAsync(id);
+
+            if (value == null)
+            {
+                return NotFound();
+            }
+
+            return value;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(User value)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(value);
+                await _context.SaveChangesAsync();
+                return Ok(value);
+
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(User value)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(value);
+                await _context.SaveChangesAsync();
+                return Ok(value);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> Delete(int id)
+        {
+            var value = await _context.Users.FindAsync(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(value);
+            await _context.SaveChangesAsync();
+
+            return value;
+        }
+    }
+}

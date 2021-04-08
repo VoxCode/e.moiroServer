8f72e3d9f6fcb1b4afe68d2666fc9c103401f474
Андрей Кезик <kezik.1992@gmail.com>
2021-04-08 22:59:22 +0300

    v 0.9.1

--- WebApplication3/Controllers/HtmlToDocxController.cs
+++ WebApplication3/Controllers/HtmlToDocxController.cs
@@ -1,4 +1,5 @@
 ﻿using e.moiroServer.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace e.moiroServer.Controllers
@@ -8,12 +9,40 @@ namespace e.moiroServer.Controllers
     public class HtmlToDocxController : ControllerBase
     {
         [AcceptVerbs("Post")]
-        public object Import(string data)
+        public byte[] Import(IFormCollection data)
         {
-            HtmlToDocxService htmlToDocxService = new HtmlToDocxService(data);
-            var docx = htmlToDocxService.Document;
-            return File(docx, "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-          "filename.docx");
+
+            IFormFile docx = data.Files[0];
+
+            string html = @"<html><head><title> XHTML to Doc  </title></head>
+                               <body><p><b>XHTML to Doc - Strict XHTML example</b></p><p></p>
+                               <table style=""margin: 5px"">
+                                  <tr>
+                                       <th> Firstname </th>
+                                       <th> Lastname </th>
+                                       <th> Age </th>
+                                  </tr>
+                                  <tr>
+                                       <td> Jill </td>
+                                       <td> Smith </td>
+                                       <td> 50 </td>
+                                  </tr>
+                                  <tr>
+                                       <td> Eve </td>
+                                       <td> Jackson </td>
+                                       <td> 94 </td>
+                                  </tr>
+                               </table>
+<p align=""center"" style=""margin - bottom:0cm; text - align:center; margin - top:0cm; margin - right:0cm; margin - left:0cm; line - height:115 %; font - size:11.0pt; font - family:&quot; Calibri & quot;,sans - serif; ""><b><span style=""font - size:14.0pt; line - height:
+115 %; font - family:&quot; Times New Roman & quot;,serif; "">ВВЕДЕНИЕ</span></b></p><p><br></p>
+                                       < p><b>Essential DocIO</b> now supports converting valid XHTML content to Doc content. The passed in content should be either XHTML 1.0 or 1.1 compliant.</p><p></p>
+                               <p><b>XHTML 1.0 Strict</b></p>
+                               <p>It is the same as HTML 4.01 Strict, but it follows XML syntax rules.</p>
+                               </body></html>";
+
+            HtmlToDocxService htmlToDocxService = new HtmlToDocxService(html, docx);
+            var resultDocx = htmlToDocxService.Document;
+            return resultDocx;
         }
     }
 }
--- WebApplication3/Services/HtmlToDocxService.cs
+++ WebApplication3/Services/HtmlToDocxService.cs
@@ -1,30 +1,63 @@
-﻿using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
-using HtmlToOpenXml;
+﻿using Microsoft.AspNetCore.Http;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
 using System.IO;
+using System.Text;
 
 namespace e.moiroServer.Services
 {
     public class HtmlToDocxService
     {
-        public HtmlToDocxService(string htmlData)
+        public HtmlToDocxService(string html, IFormFile docx)
         {
-            using MemoryStream generatedDocument = new MemoryStream();
-            using WordprocessingDocument package =
-                           WordprocessingDocument.Create(generatedDocument,
-                           WordprocessingDocumentType.Document);
-            MainDocumentPart mainPart = package.MainDocumentPart;
-            if (mainPart == null)
-            {
-                mainPart = package.AddMainDocumentPart();
-                new Document(new Body()).Save(mainPart);
-            }
-            HtmlConverter converter = new HtmlConverter(mainPart);
-            converter.ParseHtml(htmlData);
-            mainPart.Document.Save();
-            generatedDocument.Position = 0;
-            Document = generatedDocument.ToArray();
+
+            //UnicodeEncoding uniEncoding = new UnicodeEncoding();
+
+            //using (MemoryStream ms = new MemoryStream())
+            //{
+            //    var sw = new StreamWriter(ms, uniEncoding);
+            //    try
+            //    {
+            //        sw.Write(htmlData);
+            //        sw.Flush();
+            //        ms.Seek(0, SeekOrigin.Begin);
+            //    }
+            //    finally
+            //    {
+            //        sw.Dispose();
+            //    }
+            //    ms.Position = 0;
+
+            MemoryStream firstStream = new MemoryStream();
+            docx.CopyTo(firstStream);
+            firstStream.Position = 0;
+
+            WordDocument document = new WordDocument(firstStream, FormatType.Automatic);
+
+
+            document.Sections[0].Body.InsertXHTML(html);
+
+
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream, FormatType.Docx);
+            document.Close();
+            stream.Position = 0;
+
+
+            //// document.HTMLImportSettings.ImageNodeVisited += OpenImage;
+
+            //document.Open(ms, FormatType.Automatic);
+
+            //// document.HTMLImportSettings.ImageNodeVisited -= OpenImage;
+
+            //MemoryStream stream = new MemoryStream();
+
+            //document.Save(stream, FormatType.Docx);
+
+            //document.Close();
+            //stream.Position = 0;
+
+            Document = stream.ToArray();
         }
 
         public byte[] Document { get; set; }

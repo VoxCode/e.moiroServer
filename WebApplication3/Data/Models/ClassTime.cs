e441df42a4a4b0d495ad56b31afea791b05f52f7
Onokin <Aleksey.onohov@mail.ru>
2022-04-14 17:32:44 +0300

    v 0.9.4

--- WebApplication3/Data/Models/ClassTime.cs
+++ WebApplication3/Data/Models/ClassTime.cs
@@ -8,6 +8,10 @@ namespace e.moiroServer.Data.Models
         public int Id { get; set; }
         public DateTime ClassTimeStart { get; set; }
         public DateTime ClassTimeEnd { get; set; }
+        public DateTime SecondTimeStart { get; set; }
+        public DateTime SecondTimeEnd { get; set; }
+        public int Shift { get; set; }
+        public int DayOfTheWeek { get; set; }
         public List<ScheduleBlockClassTime> ScheduleBlockClassTimes { get; set; }
     }
 }
---
+++ WebApplication3/Migrations/20220414064315_AddSecondTimeAndShifts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace e.moiroServer.Migrations
+{
+    public partial class AddSecondTimeAndShifts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DayOfTheWeek",
+                table: "ClassTimes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SecondTimeEnd",
+                table: "ClassTimes",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SecondTimeStart",
+                table: "ClassTimes",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Shift",
+                table: "ClassTimes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DayOfTheWeek",
+                table: "ClassTimes");
+
+            migrationBuilder.DropColumn(
+                name: "SecondTimeEnd",
+                table: "ClassTimes");
+
+            migrationBuilder.DropColumn(
+                name: "SecondTimeStart",
+                table: "ClassTimes");
+
+            migrationBuilder.DropColumn(
+                name: "Shift",
+                table: "ClassTimes");
+        }
+    }
+}
